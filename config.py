834bb3365594c91dca1949c92a8737691dc24478
KelvinSing <143489302+KelvinSing@users.noreply.github.com>
2023-09-19 00:28:43 +0800

    config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "database-1.czfwa4nqtbvj.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "kelvinsingkaijie-employee"
+custombucket = "yapweichung-employee"
 customregion = "us-east-1"
 
